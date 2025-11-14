654997ac7cd70db40d497d98db41635cc17ec004
MrYapikZ <yafialhaidar18@gmail.com>
2025-11-14 17:17:16 +0700

    feat(sidebar): cleanup ui and add footer

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -4,6 +4,10 @@ import {
     Sidebar,
     SidebarContent,
     SidebarFooter,
+    SidebarGroup,
+    SidebarMenuButton,
+    SidebarMenuSub,
+    SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
 import {useTranslations} from "next-intl";
 import {Link} from "@/i18n/navigation";
@@ -19,29 +23,91 @@ function AppSidebar() {
                 <>
                     <Sidebar>
                         <SidebarContent>
-                            <nav className="flex flex-col gap-2 p-4">
-                                <Link href="#" className="hover:text-red-700">
-                                    {t('Navigation.NewArrivals')}
-                                </Link>
-                                <Link href="#" className="hover:text-red-700">
-                                    {t('Navigation.Collections.title')}
-                                </Link>
-                                <Link href="#" className="hover:text-red-700">
-                                    {t('Navigation.AllItems')}
-                                </Link>
-                                <Link href="#" className="hover:text-red-700">
-                                    {t('Navigation.News')}
-                                </Link>
-                                <Link href="#" className="hover:text-red-700">
-                                    {t('Navigation.About')}
-                                </Link>
-                                <Link href="#" className="text-red-500 hover:text-red-700">
-                                    {t('Navigation.Sale')}
-                                </Link>
-                            </nav>
+                            <SidebarGroup className="flex flex-col gap-2 p-4">
+                                <SidebarMenuButton>
+                                    <Link href="#" className="hover:text-red-700">
+                                        {t('Navigation.NewArrivals')}
+                                    </Link>
+                                </SidebarMenuButton>
+                                <SidebarMenuButton>
+                                    <Link href="#" className="hover:text-red-700">
+                                        {t('Navigation.Collections.title')}
+                                    </Link>
+                                </SidebarMenuButton>
+                                <SidebarMenuSub>
+                                    <SidebarMenuSubItem>
+                                        <SidebarMenuButton>
+                                            <Link href="#" className="hover:text-red-700">
+                                                {t('Navigation.Collections.items.KankyShoes')}
+                                            </Link>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuSubItem>
+                                    <SidebarMenuSubItem>
+                                        <SidebarMenuButton>
+                                            <Link href="#" className="hover:text-red-700">
+                                                {t('Navigation.Collections.items.KankySportStyle')}
+                                            </Link>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuSubItem>
+                                    <SidebarMenuSubItem>
+                                        <SidebarMenuButton>
+                                            <Link href="#" className="hover:text-red-700">
+                                                {t('Navigation.Collections.items.KankyClassic')}
+                                            </Link>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuSubItem>
+                                    <SidebarMenuSubItem>
+                                        <SidebarMenuButton>
+                                            <Link href="#" className="hover:text-red-700">
+                                                {t('Navigation.Collections.items.KankyBasketball')}
+                                            </Link>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuSubItem>
+                                    <SidebarMenuSubItem>
+                                        <SidebarMenuButton>
+                                            <Link href="#" className="hover:text-red-700">
+                                                {t('Navigation.Collections.items.KankyAthletic')}
+                                            </Link>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuSubItem>
+                                    <SidebarMenuSubItem>
+                                        <SidebarMenuButton>
+                                            <Link href="#" className="hover:text-red-700">
+                                                {t('Navigation.Collections.items.KankyNXT')}
+                                            </Link>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuSubItem>
+                                </SidebarMenuSub>
+                                <SidebarMenuButton>
+                                    <Link href="#" className="hover:text-red-700">
+                                        {t('Navigation.AllItems')}
+                                    </Link>
+                                </SidebarMenuButton>
+                                <SidebarMenuButton>
+                                    <Link href="#" className="hover:text-red-700">
+                                        {t('Navigation.News')}
+                                    </Link>
+                                </SidebarMenuButton>
+                                <SidebarMenuButton>
+                                    <Link href="#" className="hover:text-red-700">
+                                        {t('Navigation.About')}
+                                    </Link>
+                                </SidebarMenuButton>
+                                <SidebarMenuButton>
+                                    <Link href="#" className="text-red-500 hover:text-red-700">
+                                        {t('Navigation.Sale')}
+                                    </Link>
+                                </SidebarMenuButton>
+                            </SidebarGroup>
                         </SidebarContent>
-                        <SidebarFooter>
-                            {/* Footer content if needed */}
+                        <SidebarFooter className="h-64">
+                            <SidebarMenuButton className="flex flex-col h-full w-full">
+                                <Link href="#"
+                                      className="hover:text-red-700 h-full w-auto flex flex-col justify-center items-center p-2 aspect-square">
+                                    {t('Navigation.Collections.highlight.title')}
+                                    <div className="w-auto h-full aspect-square bg-red-700 rounded-2xl"/>
+                                </Link>
+                            </SidebarMenuButton>
                         </SidebarFooter>
                     </Sidebar>
                 </>
